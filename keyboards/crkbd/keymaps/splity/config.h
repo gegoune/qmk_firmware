c48ca50888bc43a326c3de454ffa7dbf30113fae
gegoune <dev@clog.rocks>
2022-06-15 13:30:51 +0200

    chore: let the journey begin

---
+++ keyboards/crkbd/keymaps/splity/config.h
@@ -0,0 +1,47 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define MASTER_LEFT
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// https://docs.qmk.fm/#/feature_auto_shift?id=retro-shift
+#define RETRO_SHIFT 500
+
+#define NO_AUTO_SHIFT_SPECIAL
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#ifdef RGB_MATRIX_ENABLE
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
