9d4b67e6556fc04fe2d965516d2ddc202329e826
gegoune <dev@clog.rocks>
2022-06-21 14:11:16 +0200

    style: format with astyle

--- keyboards/crkbd/keymaps/splity/keymap.c
+++ keyboards/crkbd/keymaps/splity/keymap.c
@@ -44,6 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NV_ALF G(KC_SPC)     // Alfred: search
 #define NV_ALFH C(G(KC_SPC)) // Alfred: clipboard history
 
+// *INDENT-OFF*
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -106,6 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+// *INDENT-ON*
 
 // Key overrides.
 #ifdef KEY_OVERRIDE_ENABLE
@@ -115,7 +117,7 @@ const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, 
 // GUI + esc = `
 const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_GUI, KC_ESC, KC_GRV);
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t **key_overrides = (const key_override_t *[]) {
     &tilde_esc_override,
     &grave_esc_override,
     NULL
@@ -125,9 +127,9 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 // Combos
 #ifdef COMBO_ENABLE
 enum combos {
-  FJ_ESC,
-  DK_COL,
-  COMBO_LENGTH
+    FJ_ESC,
+    DK_COL,
+    COMBO_LENGTH
 };
 
 uint16_t COMBO_LEN = COMBO_LENGTH;
@@ -136,17 +138,17 @@ const uint16_t PROGMEM fj_combo[] = {LCTL_T(KC_F), RCTL_T(KC_J), COMBO_END};
 const uint16_t PROGMEM dk_combo[] = {LGUI_T(KC_D), RGUI_T(KC_K), COMBO_END};
 
 combo_t key_combos[] = {
-  [FJ_ESC] = COMBO(fj_combo, KC_ESC),
-  [DK_COL] = COMBO(dk_combo, LSFT(KC_SCLN)),
+    [FJ_ESC] = COMBO(fj_combo, KC_ESC),
+    [DK_COL] = COMBO(dk_combo, LSFT(KC_SCLN)),
 };
 #endif // COMBO_ENABLE
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
-  return rotation;
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
+    return rotation;
 }
 
 #define L_BASE 0
@@ -158,24 +160,24 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
     switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
-            break;
-        case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
-            break;
-        case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
-            break;
-        case L_NAV:
-            oled_write_ln_P(PSTR("Navigate"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
-            break;
+    case L_BASE:
+        oled_write_ln_P(PSTR("Default"), false);
+        break;
+    case L_LOWER:
+        oled_write_ln_P(PSTR("Lower"), false);
+        break;
+    case L_RAISE:
+        oled_write_ln_P(PSTR("Raise"), false);
+        break;
+    case L_NAV:
+        oled_write_ln_P(PSTR("Navigate"), false);
+        break;
+    case L_ADJUST:
+    case L_ADJUST|L_LOWER:
+    case L_ADJUST|L_RAISE:
+    case L_ADJUST|L_LOWER|L_RAISE:
+        oled_write_ln_P(PSTR("Adjust"), false);
+        break;
     }
 }
 
@@ -199,7 +201,8 @@ void oled_render_logo(void) {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
         0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
         0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-        0};
+        0
+    };
     oled_write_P(crkbd_logo, false);
 }
 
