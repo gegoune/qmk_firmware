0c22fb1f4eb818f583b86951df62a8130ae7265c
gegoune <dev@clog.rocks>
2022-06-23 14:51:04 +0200

    feat: set PERMISSIVE_HOLD

--- keyboards/crkbd/keymaps/splity/config.h
+++ keyboards/crkbd/keymaps/splity/config.h
@@ -29,6 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
+#define PERMISSIVE_HOLD
+
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
