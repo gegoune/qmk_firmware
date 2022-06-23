59b6cea265efa5ce700519b0c018145786ab2c12
gegoune <dev@clog.rocks>
2022-06-23 14:38:50 +0200

    fix: cli compile needs to know what bootloader is used

--- keyboards/crkbd/keymaps/splity/rules.mk
+++ keyboards/crkbd/keymaps/splity/rules.mk
@@ -1,6 +1,7 @@
 VIA_ENABLE          = yes
 MOUSEKEY_ENABLE     = yes    # Mouse keys
 RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+BOOTLOADER          = atmel-dfu
 RGB_MATRIX_ENABLE   = yes
 OLED_ENABLE         = yes
 OLED_DRIVER         = SSD1306
