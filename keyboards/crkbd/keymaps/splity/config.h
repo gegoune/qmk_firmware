1dc47d09d4dbcb7d7bcb7cf808e516d54f1caaff
gegoune <dev@clog.rocks>
2023-09-09 11:12:37 +0200

    chore: remove now default IGNORE_MOD_TAP_INTERRUPT
    https://github.com/qmk/qmk_firmware/blob/4875bcecc2c3c66b4b259e50d6d2cf70603b3173/docs/ChangeLog/20230226.md?plain=1#L5

--- keyboards/crkbd/keymaps/splity/config.h
+++ keyboards/crkbd/keymaps/splity/config.h
@@ -9,9 +9,6 @@
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 200
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // Makes tap and hold keys trigger the hold if another key is pressed before releasing, even if it hasn't hit the TAPPING_TERM.
 #define PERMISSIVE_HOLD
 
